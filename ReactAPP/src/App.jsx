a7c5f5158468407c5c9413a2ee978ad4a300c5ab
Marwa Mohamed <marwaghonem299@gmail.com>
2026-09-05 19:47:22 +0300

    add react app

---
+++ ReactAPP/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Blog from './components/Blog'
+import About from './components/About'
+
+function App() {
+
+  return (
+    <div>
+      <Blog/>
+      <About/>
+      <h1>hello world</h1>
+    </div>
+  )
+}
+
+export default App
